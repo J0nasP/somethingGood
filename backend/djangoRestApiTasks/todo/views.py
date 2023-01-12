8037b911967251995e90faca0b97ff87498f1a6c
J0n4sP <petersen0108@gmail.com>
2023-01-12 08:52:47 +0100

    made func to make POST and GET request to db from frontend

--- backend/djangoRestApiTasks/todo/views.py
+++ backend/djangoRestApiTasks/todo/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.parsers import JSONParser 
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, JsonResponse
+from .serializers import TodoSerializer
+from .models import TodoItem
 
-# Create your views here.
+@csrf_exempt
+def todo_items(request):
+    #Get the  list of items
+    if(request.method == 'GET'):
+        todoItems = TodoItem.objects.all()
+        serializer = TodoSerializer(todoItems, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    #Post to the db
+    elif(request.method == 'POST'):
+        data = JSONParser().parse(request)
+        serializer = TodoSerializer(data=data)
+        if(serializer.is_valid()):
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.data, status=400)
